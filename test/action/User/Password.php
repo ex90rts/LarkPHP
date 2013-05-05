6237fb3af89fca58c4b7e1d5d3904b04cf57abd5
samoay <samoay@gmail.com>
2013-05-05 21:32:12 +0800

    refactory model class

---
+++ test/action/User/Password.php
@@ -0,0 +1,27 @@
+<?php
+namespace Knock\Action\User;
+
+use Flexper\Env;
+use Flexper\Action;
+use Flexper\Constants;
+
+class Password extends Action{
+
+	function init(){
+		$this->rules = array(
+			array(parent::VALID_REQUIRED, 'username', 'password'),
+			array(parent::VALID_REGEX, '/^\w{5,20}$/', 'username'),
+			array(parent::VALID_REGEX, '/^\w{6,16}$/', 'password'),
+		);
+	}
+
+    function execute (){
+        echo "Username: ".$this->request->username."<br />";
+        echo "Password: ".$this->request->password."<br />";
+        $this->model('User')->update(array('username'=>$this->request->username), array('password'=>$this->request->password));
+    }
+
+    function finish (){
+
+    }
+}
