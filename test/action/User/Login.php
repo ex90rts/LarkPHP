6e8b5932ab334c0aa0cd2fc48390eb7803369eb5
U-samoay-PC\samoay <samoay@samoay-PC.(none)>
2013-06-02 13:31:05 +0800

    add markitup editor

--- test/action/User/Login.php
+++ test/action/User/Login.php
@@ -5,13 +5,19 @@ use Flexper\Action;
 
 class Login extends Action{
 	function execute(){
-		$this->response->setSession('name', 'Samoay');
-		$this->response->setSession('group', 'user');
-		$this->response->setSession('permissions', array('User_Get'));
-
-		$this->response->text('Login Succed');
-		sleep(5);
-		$this->redirect();
+		if (empty($this->request['LoginForm'])){
+			$response = $this->response;
+			$response->tab = 'LOGIN';
+			$response->template('user/login.php');
+		}else{
+			$this->response->setSession('name', 'Samoay');
+			$this->response->setSession('group', 'user');
+			$this->response->setSession('permissions', array('User_Get'));
+	
+			$this->response->text('Login Succed');
+			sleep(5);
+			$this->redirect();
+		}
 	}
 
 	function redirect(){
