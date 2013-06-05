f2aacfceedd9c79bb7ca454677cc618cf166250c
U-samoay-PC\samoay <samoay@samoay-PC.(none)>
2013-06-06 00:26:08 +0800

    up

--- test/template/index.php
+++ test/template/index.php
@@ -15,25 +15,24 @@
 <div class="contentBox">
 	<div class="contentLeft">
     	<div class="articleNode">
-        	<p>$article = new Article(1);</p>
-            <p>$article->show();</p>
-            <p>//echo $article->title;</p>
-            <p><h3><a class="articleTitle" href="#">Nodejs擅长什么？</a> </h3></p>
-            <p>//echo substr($article->content, 0, 100);</p>
-            <p>Node给前端开发者打了鸡血，现在我们可以用一种语言来写前后端的程序了，但我们都要避免的一点是，当你有一把锤子，你看见什么都是钉子。
-    所以我们不应该为了使用js而js，也不要为了时髦而使用Node。我们应该搞清楚，Node适合哪些场景，在这些场景下，Node是否是更优的选择？比起某一门“正常”的服务器语言？
-    Node更擅长……Node更擅长处理并发连接。这是由Node处理连接的方法决定的。
-    Node公开宣称的目标是 “旨在提供一种简单的构建可伸缩网络程序......
-    		</p>
-            <p>//outputArray($article->tags);</p>
+            <div class="articleTitle"><div class="articleID">#45634</div><h3><a href="#">Nodejs擅长什么？</a></h3></div>
+            <div class="articleContent">
+                <p>Node给前端开发者打了鸡血，现在我们可以用一种语言来写前后端的程序了，但我们都要避免的一点是，当你有一把锤子，你看见什么都是钉子。
+        所以我们不应该为了使用js而js，也不要为了时髦而使用Node。我们应该搞清楚，Node适合哪些场景，在这些场景下，Node是否是更优的选择？比起某一门“正常”的服务器语言？
+        Node更擅长……Node更擅长处理并发连接。这是由Node处理连接的方法决定的。
+        Node公开宣称的目标是 “旨在提供一种简单的构建可伸缩网络程序......
+                </p>
+            </div>
             <div class="articleInfo">
+                <div class="articleTags">
                     <a href="#">PHP</a>
                     <a href="#">Javascript</a>
                     <a href="#">Java</a>
                     <a href="#">MySQL</a>
+                </div>
+                <div class="articleTime">2013-06-05 22:00</div>
+                <div style="clear:both"></div>
             </div>
-            <p>$article->save();</p>
-            <p>$article->save();</p>
     	</div>
     </div>
     
