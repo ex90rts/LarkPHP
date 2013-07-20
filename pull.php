b79025780332013de10113cf473830c60f7df9b3
samoay@gmail.com <samoay@gmail.com>
2013-07-20 16:04:49 +0800

    auto sync test

--- pull.php
+++ pull.php
@@ -1,3 +1,3 @@
 <?php
-//this is a git hook script just pull master branch, enjoy ;)
+//this is a git hook script just pull master branch, have fun ;)
 echo nl2br(shell_exec('sudo git pull origin master 2>&1'));
